90d672ed82cdcc9b481ebcbfa80371faab6f1901
josemoura212 <117314226+josemoura212@users.noreply.github.com>
2023-10-21 14:03:08 -0300

    The diff changes the type of `nomeFuture` from `Future<String>` to `ObservableFuture<String>`. It also changes the assignment of `nomeFuture` to use `ObservableFuture.value("")` instead of `Future.value("").asObservable()`. Finally, it changes the assignment of `nomeFuture` in the `buscarNome` method to use `ObservableFuture(buscaNomeFuture)` instead of `buscaNomeFuture.asObservable()`. (generated by useblackbox.ai )

--- lib/observables/future/future_controller.dart
+++ lib/observables/future/future_controller.dart
@@ -5,11 +5,18 @@ class FutureController = FutureControllerBase with _$FutureController;
 
 abstract class FutureControllerBase with Store {
   @observable
-  var nomeFuture = Future.value("").asObservable();
+  // var nomeFuture = Future.value("").asObservable();
+  var nomeFuture = ObservableFuture.value("");
 
   Future<void> buscarNome() async {
-    nomeFuture =
-        Future.delayed(const Duration(seconds: 2), () => "José Augusto")
-            .asObservable();
+    // nomeFuture =
+    //     Future.delayed(const Duration(seconds: 2), () => "José Augusto")
+    //         .asObservable();
+
+    final buscaNomeFuture =
+        Future.delayed(const Duration(seconds: 2), () => "José Augusto");
+
+    // nomeFuture = buscaNomeFuture.asObservable();
+    nomeFuture = ObservableFuture(buscaNomeFuture);
   }
 }
